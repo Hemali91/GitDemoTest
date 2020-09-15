7211f5d93c91cf3f2cf7f8bd1a7e243b353cfb6a
hemali <hemali@localhost>
2020-09-15 10:19:40 -0700

    first commit of project.

---
+++ GitDemo/src/main/java/com/apex/gitDemo/App.java
@@ -0,0 +1,13 @@
+package com.apex.gitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ GitDemo/src/test/java/com/apex/gitDemo/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.apex.gitDemo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
